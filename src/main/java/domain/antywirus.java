1b5cd4b94756d76820d918b553f524e30d725a57
supperbull <knicio123@gmail.com>
2017-01-10 21:48:50 +0100

    Dodanie zmiennych oraz QueryStatement w domain.antywirus oraz domain.pakiet

--- src/main/java/domain/antywirus.java
+++ src/main/java/domain/antywirus.java
@@ -3,5 +3,65 @@ package domain;
 /**
  * Created by Redbullek on 2017-01-10.
  */
+
+import java.util.Collection;
+import javax.persistence.*;
+
+
+@Entity
+@NamedQueries({
+        @NamedQuery(name = "antywirus.all", query = "Select a from antywirus a"),
+        @NamedQuery(name = "antywirus.byNazwa",query = "select a from antywirus a where a.nazwaantywirus=:name")
+
+})
 public class antywirus {
+
+    private Long id_antywirus;
+    private String nazwaantywirus;
+    private String opis;
+    private double ocena;
+    private Collection<pakiet> pakiety;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name ="IDantywirus")
+    public Long getId() {
+        return id_antywirus;
+    }
+
+    public void setId(Long id_antywirus) {
+        this.id_antywirus = id_antywirus;
+    }
+    @Column(unique = true,nullable = false)
+    public String getnazwaantywirus() {
+        return nazwaantywirus;
+    }
+
+    public void setnazwaantywirus(String nazwaantywirus) {
+        this.nazwaantywirus = nazwaantywirus;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
+
+    public double getocena() {
+        return ocena;
+    }
+
+    public void setocena(double ocena) {
+        this.ocena = ocena;
+    }
+    @OneToMany(mappedBy = "category",cascade = CascadeType.PERSIST,fetch = FetchType.LAZY)
+    public Collection<pakiet> getPakiety() {
+        return pakiety;
+    }
+
+    public void setModels(Collection<pakiet> pakiety) {
+        this.pakiety = pakiety;
+    }
 }
--- src/main/java/domain/pakiet.java
+++ src/main/java/domain/pakiet.java
@@ -1,7 +1,67 @@
 package domain;
 
+import domain.antywirus;
+
+import javax.persistence.*;
+
 /**
- * Created by Redbullek on 2017-01-10.
+ * Created by GSun12 on 21.12.2016.
  */
+@Entity
+@NamedQueries({
+        @NamedQuery(name="model.all", query = "select p from pakiet p"),
+        @NamedQuery(name = "model.byfunkcje",query = "select p from pakiet p where p.funkcje=:funkcje "),
+        @NamedQuery(name = "model.byantywirus",query = "select b from pakiet b where b.antywirus=:antywirus")
+})
 public class pakiet {
+
+    private Long id_pakiet;
+    private String nazwapakiet;
+    private String funkcje;
+    private Double cena;
+    private antywirus antywirus;
+
+    @Id
+    @Column(name = "IDModel")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId() {
+        return id_pakiet;
+    }
+
+    public void setId(Long id_pakiet) {
+        this.id_pakiet = id_pakiet;
+    }
+
+    public String getNazwapakiet() {
+        return nazwapakiet;
+    }
+
+    public void setNazwapakiet(String nazwapakiet) {
+        this.nazwapakiet = nazwapakiet;
+    }
+    @Column(unique = true,nullable = false)
+    public String getfunkcje() {
+        return funkcje;
+    }
+
+    public void setfunkcje(String funkcje) {
+        this.funkcje = funkcje;
+    }
+
+    public String getcena() {
+        return cena;
+    }
+
+    public void setcena(String cena) {
+        this.cena = cena;
+    }
+    @ManyToOne
+    @JoinColumn(name = "antywirus_IDantywirus")
+    public antywirus getAntywirus() {
+        return antywirus;
+    }
+
+    public void setAntywirus(antywirus antywirus) {
+        this.antywirus = antywirus;
+    }
 }
